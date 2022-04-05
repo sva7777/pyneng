6f29249c9d21747b7291c54a648773a351d0b35d
vasily <sva7777@gmail.com>
2022-04-05 11:23:08 +0300

    one exercise

--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -30,3 +30,26 @@ Out[1]: '11111111111111111111111111110000'
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+address = input("введите IPv4-сеть в формате 10.1.1.0/24 :")
+separator = address.split("/")
+ip_separator = separator[0].split(".")
+bulk = '''
+Network:
+{:10}{:10}{:10}{:10}
+{:10}{:10}{:10}{:10}'''
+print(bulk.format( ip_separator[0],ip_separator[1], ip_separator[2], ip_separator[3], format(int(ip_separator[0]),"08b"), format(int(ip_separator[1]),"08b"), format(int(ip_separator[2]),"08b"), format(int(ip_separator[3]),"08b")   ) )
+bulk_mask = '''
+Mask:
+/{}
+{:10}{:10}{:10}{:10}
+{:10}{:10}{:10}{:10}
+'''
+bin_mask = "1"*int(separator[1])+"0"*(32-int(separator[1]))
+bin_mask_a = bin_mask[0:8]
+bin_mask_b = bin_mask[8:16]
+bin_mask_c = bin_mask[16:24]
+bin_mask_d = bin_mask[24:32]
+print(bulk_mask.format(separator[1], str(int(bin_mask_a,2)), str(int(bin_mask_b,2)), str(int(bin_mask_c,2)), str(int(bin_mask_d,2)), bin_mask_a, bin_mask_b, bin_mask_c, bin_mask_d ) )
+
+
+
