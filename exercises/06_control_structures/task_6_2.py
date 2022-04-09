857c41f684e6bf88cea3ca1f38d42461dd90797d
sva7777 <sva7777@gmail.com>
2022-04-09 21:41:09 +0300

    some exersises

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -12,3 +12,17 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip_string = input("введите IP адрес в формате 10.0.1.1 :")
+ip_a,ip_b,ip_c,ip_d = ip_string.split(".")
+
+if ( int(ip_a) in range(1,224) ):
+    print("unicast")
+elif (int(ip_a) in range (224,240)):
+    print("multicast")
+elif ( int(ip_a)==255 and int(ip_b) == 255 and int (ip_c) == 255 and int(ip_d) ==255):
+    print("local broadcast")
+elif ( int(ip_a) == 0 and int(ip_b) == 0 and int(ip_c) == 0 and int(ip_d) == 0):
+    print("unassigned")
+else:
+    print("unused")
--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,24 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip_string = input("введите IP адрес в формате 10.0.1.1 :")
+
+if ip_string.count(".") == 3:
+    ip_a,ip_b,ip_c,ip_d = ip_string.split(".")
+    if  (not ip_a.isdigit() )   or (not ip_b.isdigit() ) or (not ip_c.isdigit() ) or (not ip_d.isdigit() ) :
+        print("Неправильный IP-адрес")
+    elif ( ( not int(ip_a) in range (0,256)) or (not int(ip_b) in range (0,256)) or (not int(ip_c) in range (0,256))  or (not int(ip_d) in range (0,256))):
+        print("Неправильный IP-адрес")
+    elif ( int(ip_a) in range(1,224) ):
+        print("unicast")
+    elif (int(ip_a) in range (224,240)):
+        print("multicast")
+    elif ( int(ip_a)==255 and int(ip_b) == 255 and int (ip_c) == 255 and int(ip_d) ==255):
+        print("local broadcast")
+    elif ( int(ip_a) == 0 and int(ip_b) == 0 and int(ip_c) == 0 and int(ip_d) == 0):
+        print("unassigned")
+    else:
+        print("unused")
+else:
+    print("Неправильный IP-адрес")
