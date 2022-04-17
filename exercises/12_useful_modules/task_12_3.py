3fede0ab112ae59aff8309b5ada867555569f838
sva7777 <sva7777@gmail.com>
2022-04-17 20:49:22 +0300

    one test

--- exercises/12_useful_modules/task_12_3.py
+++ exercises/12_useful_modules/task_12_3.py
@@ -18,3 +18,15 @@ Reachable    Unreachable
              10.1.1.9
 
 """
+from tabulate import tabulate
+
+reach= ["10.1.1.1", "10.1.1.2"]
+un_reach = ["10.1.1.7", "10.1.1.8", "10.1.1.9"]
+
+def print_ip_table(reachable, unreachable):
+    table = {"Reachable": reachable, "Unreachable": unreachable}
+    
+    print ( tabulate(table, headers="keys")  )
+
+
+print_ip_table(reach, un_reach)
