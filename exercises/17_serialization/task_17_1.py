0488997281ca8501b49c3358fc62f523bae46a58
sva7777 <sva7777@gmail.com>
2022-05-03 17:11:16 +0300

    one test

--- exercises/17_serialization/task_17_1.py
+++ exercises/17_serialization/task_17_1.py
@@ -31,3 +31,24 @@ sw3,00:E9:22:11:A6:50,100.1.1.7,3,FastEthernet0/21
 sw2_dhcp_snooping.txt, sw3_dhcp_snooping.txt.
 
 """
+import re 
+import csv
+def write_dhcp_snooping_to_csv(filenames, output):
+    r_string= (r"(?P<MAC>\S+) *(?P<ip>\S+) *(\d+) *(\S+) *(?P<vlan>\d+) *(?P<interface>\S+)")
+    re_comp = re.compile(r_string)
+    
+    res_list =  [ ["switch", "mac","ip","vlan","interface"] ]
+    
+    for filename in filenames:
+        device_name = filename.split("_")[0]
+        with open("/home/vasily/pyneng/exercises/17_serialization/"+filename, "r") as f:
+            for m in re.finditer(re_comp, f.read() ):
+                res_list.append( [device_name, m.group('MAC'), m.group('ip'), m.group('vlan'), m.group('interface') ] ) 
+    
+    with open(output, "w") as f:
+        wr = csv.writer(f)
+        wr.writerows(res_list)
+    print(res_list)
+
+file_names_list = ["sw1_dhcp_snooping.txt", "sw2_dhcp_snooping.txt", "sw3_dhcp_snooping.txt" ]
+write_dhcp_snooping_to_csv(file_names_list, "test")
