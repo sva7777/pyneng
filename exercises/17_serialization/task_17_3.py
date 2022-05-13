3c37a798a41cac67d7b7815e9683f1dd89b46c94
sva7777 <sva7777@gmail.com>
2022-05-13 16:52:07 +0300

    one test

--- exercises/17_serialization/task_17_3.py
+++ exercises/17_serialization/task_17_3.py
@@ -24,3 +24,38 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 Проверить работу функции на содержимом файла sh_cdp_n_sw1.txt
 """
+import re
+def parse_sh_cdp_neighbors (input_str):
+    res = dict()
+    r_string = r"(\S+)[>#]"
+    match = re.search(r_string, input_str, re.DOTALL)
+    if not match:
+        return None
+        
+    device_name = match.group(1)
+    
+    
+    
+    r_string = (r"(?P<device_id>\S+) *(?P<local_int>\S+ \S+) *"\
+                "(\d+) *\S \S \S *(\d+) *(?P<port_int>\S+ \S+)")
+    re_comp = re.compile(r_string)
+    middle_dict = dict()
+    for m in re.finditer(re_comp, input_str ):
+        device_id, local_int, port_int= m.group("device_id", "local_int", "port_int")
+        small_dict = dict()
+        small_dict[device_id] = port_int
+        middle_dict[local_int] = small_dict
+        
+        
+    res[device_name] = middle_dict
+
+    return res
+
+    
+
+file_name= "/home/vasily/pyneng/exercises/17_serialization/sh_cdp_n_sw1.txt"
+
+with open(file_name, "r") as f:
+    file_context = f.read()
+    parse_sh_cdp_neighbors(file_context)
+    
