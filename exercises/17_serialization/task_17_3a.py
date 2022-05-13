45e8040462b0c6ddbaf72ad45f270208ac9b5392
sva7777 <sva7777@gmail.com>
2022-05-13 18:05:34 +0300

    resolve task 17_3a

--- exercises/17_serialization/task_17_3a.py
+++ exercises/17_serialization/task_17_3a.py
@@ -36,3 +36,53 @@
 в файл topology.yaml. Он понадобится в следующем задании.
 
 """
+
+import re
+from pprint import pprint
+import yaml
+
+def parse_sh_cdp_neighbors (input_str):
+    r_string = r"(\S+)[>#]"
+    match = re.search(r_string, input_str, re.DOTALL)
+    if not match:
+        return None
+        
+    device_name = match.group(1)
+    
+        
+    r_string = r"(?P<device_id>\w+) +(?P<local_int>\S+ \S+) +\d+ +[\w ]+ +\S+ +(?P<port_int>\S+ \S+)"
+    re_comp = re.compile(r_string)
+    middle_dict = dict()
+    for m in re.finditer(re_comp, input_str ):
+        device_id, local_int, port_int= m.group("device_id", "local_int", "port_int")
+        small_dict = dict()
+        small_dict[device_id] = port_int
+        middle_dict[local_int] = small_dict
+     
+    
+    return (device_name,middle_dict)
+
+
+def generate_topology_from_cdp(list_of_files, save_to_filename = None):
+    res = dict()
+    for file_name in list_of_files:
+        with open("/home/vasily/pyneng/exercises/17_serialization/"+file_name,"r") as f:
+            print(file_name)
+            device,details = parse_sh_cdp_neighbors(f.read())
+            res[device] = details
+    
+    if save_to_filename:
+        with open(save_to_filename, "w") as f:
+            yaml.dump(res,f)
+    
+    return res
+        
+files_list = {"sh_cdp_n_sw1.txt", 
+              "sh_cdp_n_r1.txt",  
+              "sh_cdp_n_r2.txt",
+              "sh_cdp_n_r3.txt",
+              "sh_cdp_n_r4.txt",
+              "sh_cdp_n_r5.txt",
+              "sh_cdp_n_r6.txt"}
+
+generate_topology_from_cdp(files_list, "/home/vasily/pyneng/exercises/17_serialization/test.txt")
