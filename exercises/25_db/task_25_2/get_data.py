210b87a0498a5f10b0ccedbbd534c044e160663a
vasily <sva7777@gmail.com>
2022-06-03 16:26:25 +0300

    continue task_25_2

--- exercises/25_db/task_25_2/get_data.py
+++ exercises/25_db/task_25_2/get_data.py
@@ -1,9 +1,34 @@
 import sys
+import sqlite3
+
+from pprint import pprint
+
+def get_table_columns_name(conn, table_name):
+    r = []
+    sql_query = "PRAGMA table_info({})"
+    res= conn.execute(sql_query.format(table_name) )
+    for line in res:
+        r.append( line[1] )
+        
+    return r
+    
+    
+
+db_file_name= "/home/vasily/pyneng/exercises/25_db/task_25_1/task_25_1.db"
 
 if __name__ == '__main__':
     arg_count = len(sys.argv)
     
-    if arg_count != 1 or arg_count !=3:
+    if arg_count != 1 and arg_count !=3:
         print("Пожалуйста, введите два или ноль аргументов")
-    else
-        # обрабатать кейсы одного и двух параметров
+    else:
+        with sqlite3.connect(db_file_name) as conn:
+            if arg_count ==3:
+                columns = get_table_columns_name(conn, "dhcp")
+                if argv[1] not in columns:
+                    pprint("Данный параметр не поддерживается.")
+                    #Допустимые значения параметров: mac, ip, vlan, interface, switch
+
+            
+        
+        
