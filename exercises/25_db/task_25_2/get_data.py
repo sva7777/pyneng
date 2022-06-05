a26941bc0491edba2e59b6c67dedccc6de054ea1
sva7777 <sva7777@gmail.com>
2022-06-05 14:06:33 +0300

    resolve task_25_2

--- exercises/25_db/task_25_2/get_data.py
+++ exercises/25_db/task_25_2/get_data.py
@@ -1,7 +1,7 @@
 import sys
 import sqlite3
+from tabulate import tabulate
 
-from pprint import pprint
 
 def get_table_columns_name(conn, table_name):
     r = []
@@ -11,8 +11,26 @@ def get_table_columns_name(conn, table_name):
         r.append( line[1] )
         
     return r
+
+def show_table_data_helper(conn, table_name, column_name =None , value =None):
+    sql_query = "SELECT * from dhcp"
+    if column_name != None :
+        sql_query = sql_query +" WHERE " + column_name +" = ?"
+        res = conn.execute(sql_query, (value,) )
+    else:
+        res = conn.execute(sql_query)
     
+    print(tabulate(res))
     
+
+def show_table_data(conn, table_name, column_name = None, value = None):
+    # отобразить правильный заголовок
+    if column_name is None:
+        print("В таблице {} такие записи:".format(table_name) )
+    else:
+        print("Информация об устройствах с такими параметрами: {} {}".format(column_name, value) )
+        
+    show_table_data_helper(conn, table_name, column_name, value)
 
 db_file_name= "/home/vasily/pyneng/exercises/25_db/task_25_1/task_25_1.db"
 
@@ -25,10 +43,14 @@ if __name__ == '__main__':
         with sqlite3.connect(db_file_name) as conn:
             if arg_count ==3:
                 columns = get_table_columns_name(conn, "dhcp")
-                if argv[1] not in columns:
-                    pprint("Данный параметр не поддерживается.")
-                    #Допустимые значения параметров: mac, ip, vlan, interface, switch
-
-            
-        
-        
+                if sys.argv[1] not in columns:
+                    print("Данный параметр не поддерживается.")
+                    # Отобразить поддерживаемые колонки
+                    print("Допустимые значения параметров: {}".format(columns))
+                else:
+                    column_name = sys.argv[1]
+                    value= sys.argv[2]
+                    show_table_data(conn, "dhcp", column_name, value)
+            else: 
+                show_table_data(conn, "dhcp")
+                
